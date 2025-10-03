7a30b03a5073725985496fb9647beef46d888d39
Luciano Villanueva <lucianoivillanuevar@gmail.com>
2025-10-02 21:25:49 -0300

    fix de validaciones

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -6,16 +6,10 @@ import {
   updatePrivateProfile,
   deletePrivateProfile,
 } from "../controllers/profile.controller.js";
-import { validarCrearUsuario } from "../validations/usuario.validation.js";
+import { validarUpdateProfile } from "../validations/usuario.validation.js"; 
 const router = Router();
 router.get("/public", getPublicProfile);
 router.get("/private", authMiddleware, getPrivateProfile);
-router.patch("/private", authMiddleware, validarCrearUsuario, updatePrivateProfile);
+router.patch("/private", authMiddleware, validarUpdateProfile, updatePrivateProfile);
 router.delete("/private", authMiddleware, deletePrivateProfile);
-
 export default router;
-
-
-
-
-
